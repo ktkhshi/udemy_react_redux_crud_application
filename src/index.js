93ea883b50c43f2a138a9d0d981cc62daa034e84
ktkhshi <ktkhshi@gmail.com>
2024-02-13 22:23:27 +0900

    implement events index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore as createStore } from 'redux'
-import { Provider } from 'react-redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { thunk } from 'redux-thunk';
 
 import './index.css';
 import reducer from './reducers'
 
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <EventsIndex />
     </React.StrictMode>
   </Provider>
 );
