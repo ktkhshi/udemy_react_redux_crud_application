d970d30ef7fa5f9042b3ff9bf61ef5cdedb20e99
ktkhshi <ktkhshi@gmail.com>
2024-02-19 20:59:53 +0900

    implement delete feature

---
+++ src/components/withRouter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+export const withRouter = (WrappedComponent) => (props) => {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  return <WrappedComponent {...props} params={params} navigate={navigate} />;
+};
+
+  export default withRouter;
--- src/index.js
+++ src/index.js
@@ -4,26 +4,32 @@ import { legacy_createStore as createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { thunk } from 'redux-thunk';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import './index.css';
 import reducer from './reducers'
 
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new'; 
+import EventsShow from './components/events_show'; 
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/events/new" element={<EventsNew />} />
-          <Route path="/" element={<EventsIndex />} />
-        </Routes>
-      </Router>
-    </Provider>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/events/new" element={<EventsNew />} />
+        <Route path="/events/:id" element={<EventsShow/>} />
+        <Route path="/" element={<EventsIndex />} />
+        <Route path="/events" element={<EventsIndex />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
