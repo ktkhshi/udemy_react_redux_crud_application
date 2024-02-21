56ee837f0366efd49cab7da112f230af8fc8d82f
ktkhshi <ktkhshi@gmail.com>
2024-02-21 18:32:48 +0900

    apply material-ui eventsIndex

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -5,6 +5,15 @@ import { Link } from "react-router-dom";
 
 import { readEvents } from "../actions";
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
 class EventsIndex extends Component {
   componentDidMount() {
     this.props.readEvents()
@@ -12,34 +21,50 @@ class EventsIndex extends Component {
   
   renderEvents() {
     return _.map(this.props.events, event => (
-      <tr key={event.id}>
-        <td>{event.id}</td>
-        <td>
+      <TableRow key={event.id}>
+        <TableCell>{event.id}</TableCell>
+        <TableCell>
           <Link to={`/events/${event.id}`} >
             {event.title}
           </Link>
-          </td>
-        <td>{event.body}</td>
-      </tr>
+          </TableCell>
+        <TableCell>{event.body}</TableCell>
+      </TableRow>
     ))
   }
   
   render() {
+    const style = {
+      margin: 0,
+      top: 'auto',
+      right: 20,
+      bottom: 20,
+      left: 'auto',
+      position: 'fixed',
+    }
     return (
       <React.Fragment>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Body</th>
-              </tr>
-            </thead>
-            <tbody>
+          <Fab href="/events/new" className="classes.fab"
+          sx={{
+            position: "fixed",
+            bottom: (theme) => theme.spacing(2),
+            right: (theme) => theme.spacing(2)
+          }}
+          color="primary">
+            <AddIcon />
+          </Fab>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Title</TableCell>
+                <TableCell>Body</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
               {this.renderEvents()}
-            </tbody>
-          </table>
-          <Link to="/events/new">New Event</Link>
+            </TableBody>
+          </Table>
       </React.Fragment>
     )
   }
